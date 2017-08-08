747709547915031c615d110a4aefe2bc9195a36a
Liara A. M. Rørvig <30539548+AsynchronousToddler@users.noreply.github.com>
2017-08-09 00:02:42 +0200

    Added app starter and discord setup

---
+++ app.js
@@ -0,0 +1,42 @@
+/**
+ * Created by Liara Anna Maria Rørvig on 08/08/2017.
+ */
+'use strict';
+
+const AsynchronousSelfBot = require( './src/index.js' );
+const fs              = require( 'fs' );
+const path            = require( 'path' );
+
+const DEFAULT_CONFIG_FILE = './config.json';
+
+let process_arguments     = process.argv.slice( 2 );
+let config_file           = process_arguments.length > 0 ? process_arguments[ 0 ] : DEFAULT_CONFIG_FILE;
+let full_config_file_path = path.join( __dirname, config_file );
+
+fs.readFile( full_config_file_path, ( file_read_error, file_content )=> {
+    if( file_read_error ) {
+        console.log( file_read_error.stack );
+
+        return process.exit( 1 );
+    }
+
+    let configuration;
+
+    try {
+        configuration = JSON.parse( file_content );
+    } catch( file_content_parse_error ) {
+        console.log( file_content_parse_error.stack );
+
+        return process.exit( 1 );
+    }
+
+    let app = new AsynchronousSelfBot( configuration );
+
+    app.on( 'error', ( error ) => {
+        console.log( error.stack );
+
+        process.exit( 1 );
+    } );
+
+    app.start( );
+} );
--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@
 'use strict';
 
 const DiscordClient    = require( 'discord.js' ).Client;
+const dot              = require( 'dot-object' );
 const EventEmitter     = require( 'events' ).EventEmitter;
-const Request          = require( 'request' );
+const request          = require( 'request' );
 const OptionsValidator = require( './utils/options_validator.js' );
+
+const OPTION_DISCORD_TOKEN = 'tokens.discord';
 
 class AsynchronousSelfBot extends EventEmitter {
     constructor( options ) {
@@ -20,8 +23,35 @@ class AsynchronousSelfBot extends EventEmitter {
             throw new Error( error_message );
         }
 
-        this.options = options || { };
+        this.options        = options;
+
+        this.setupDiscord();
     }
+
+    start( ) {
+        this.discord_client.login( dot.pick( OPTION_DISCORD_TOKEN, this.options ) );
+    }
+
+    //
+    //Event Handlers Start: Discord
+    //
+
+    onEventDiscordReady( ) {
+        this.emit( 'ready', true );
+    }
+
+    //
+    //Event Handlers End: Discord
+    //
+
+    setupDiscord() {
+        this.discord_client = new DiscordClient( );
+
+        this.discord_client.on( 'ready', ( ) => {
+            this.onEventDiscordReady( );
+        } );
+    }
+
 }
 
 module.exports = AsynchronousSelfBot;
