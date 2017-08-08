215e5253c1663d6f4c689d0e3a539d4e0697ebf2
Liara A. M. Rørvig <30539548+AsynchronousToddler@users.noreply.github.com>
2017-08-09 00:12:21 +0200

    Minor formatting

--- app.js
+++ app.js
@@ -4,8 +4,8 @@
 'use strict';
 
 const AsynchronousSelfBot = require( './src/index.js' );
-const fs              = require( 'fs' );
-const path            = require( 'path' );
+const fs                  = require( 'fs' );
+const path                = require( 'path' );
 
 const DEFAULT_CONFIG_FILE = './config.json';
 
